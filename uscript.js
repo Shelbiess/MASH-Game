8f807761e7f4c6e06e2c93d6f20f34dbbee421f7
Shelbiess <ssittre@student.alamo.edu>
2023-02-10 16:17:24 -0600

    changed

--- uscript.js
+++ uscript.js
@@ -8,15 +8,22 @@
 
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
+</body> 
+</html><!DOCTYPE html>
+<html>
+<body>
+
+<h2>What Can JavaScript Do?</h2>
+
+<p>JavaScript can change HTML attribute values.</p>
+
+<p>In this case JavaScript changes the value of the src (source) attribute of an image.</p>
+
+<button onclick="document.getElementById('myImage').src='pic_bulbon.gif'">Turn on the light</button>
+
+<img id="myImage" src="pic_bulboff.gif" style="width:100px">
+
+<button onclick="document.getElementById('myImage').src='pic_bulboff.gif'">Turn off the light</button>
+
 </body>
-</html><html>
-    <head>
-        <meta name="viewport" content="width=device-width,initial-scale=1">
-        <title>CRP</title>
-        <link rel="stylesheet" href="styles.css">
-    </head>
-    <body>
-        <!-- content -->
-        <script src="script.js"></script>
-    </body>
 </html>
