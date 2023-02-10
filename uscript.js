c3e1c0b2211afc31795e53c089f90b8b5cd986d4
Shelbiess <ssittre@student.alamo.edu>
2023-02-10 16:08:50 -0600

    littlechange

--- uscript.js
+++ uscript.js
@@ -9,4 +9,14 @@
 <button type="button" onclick='document.getElementById("demo").innerHTML = "Hello JavaScript!"'>Click Me!</button>
 
 </body>
+</html><html>
+    <head>
+        <meta name="viewport" content="width=device-width,initial-scale=1">
+        <title>CRP</title>
+        <link rel="stylesheet" href="styles.css">
+    </head>
+    <body>
+        <!-- content -->
+        <script src="script.js"></script>
+    </body>
 </html>
